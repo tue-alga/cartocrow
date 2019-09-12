d0983a9f973051fe4e71ebed22acbde4ada3d983
thijsvl <t.vanlankveld@esciencecenter.nl>
2019-09-12 14:11:38 +0200

    Added template for PHP files to run the native executables.

---
+++ wwwroot/script/template.php
@@ -0,0 +1,10 @@
+<!--Note that while we could have one PHP file that takes as input the executable to run, this would pose an enormous security risk.-->
+<?php
+    // Collect the command line arguments and separate them by spaces.
+    $args = $_REQUEST["args"];
+    $delim = isset($_REQUEST["delim"]) ? $_REQUEST["delim"] : "|";
+    $arg_str = str_replace($delim, " ", $args);
+    // Run the executable and return what it sent to the standard output stream.
+    $result = shell_exec("/bin/executable $arg_str");
+    echo $result;
+?>
