1d31578d4133ce3bb7e17e9f21f012a17463d488
thijsvl <t.vanlankveld@esciencecenter.nl>
2019-09-12 12:05:04 +0200

    Initial version that correctly runs native executables through PHP.

---
+++ CHANGELOG.md
@@ -0,0 +1,8 @@
+=== VERSION 0.1.1.0 ===
+
+* Working version with PHP-powered executable.
+
+=== VERSION 0.1.0.0 ===
+
+* Initial implementation of website layout.
+
--- src/console/necklace_map/necklace_map.cpp
+++ src/console/necklace_map/necklace_map.cpp
@@ -7,17 +7,27 @@
 #include "console/necklace_map/internal/test_internal.h"
 
 int main(int argc, char** argv) {
-    std::cout << "Args: " << std::endl;
+    std::cerr << "Args: " << std::endl;
     for ( int i = 1; i < argc; ++i )
-        std::cout << "\t" << argv[i] << std::endl;
+        std::cerr << "\t" << argv[i] << std::endl;
 
-    std::cout << "Necklace map: " << geoviz::proc_necklace_map() << std::endl;
-    std::cout << "Flow Diagram: " << geoviz::proc_flow_diagram() << std::endl;
+    std::cerr << "Necklace map: " << geoviz::proc_necklace_map() << std::endl;
+    std::cerr << "Flow Diagram: " << geoviz::proc_flow_diagram() << std::endl;
 
     // TODO(tvl) place in 'usage' print.
-    std::cout << "GeoViz version: " << GEOVIZ_VERSION << std::endl;
+    std::cerr << "GeoViz version: " << GEOVIZ_VERSION << std::endl;
 
-    std::cout << "Internal number: " << internal::test() << std::endl;
+    std::cerr << "Internal number: " << internal::test() << std::endl;
+
+    std::cout << "<div>";
+    std::cout << "<!--To be loaded as support card.-->";
+    std::cout << "<h1>Application-based component</h1>";
+    std::cout << "<p>This page is just to test running a server-side application using PHP.</p>";
+    std::cout << "<p>The following command line arguments were caught:<br>";
+    for ( int i = 1; i < argc; ++i )
+        std::cout << argv[i] << "<br>";
+    std::cout << "</p>";
+    std::cout << "</div>";
 
     return 0;
 }
