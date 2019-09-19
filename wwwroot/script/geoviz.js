d1c2a1cb38b95d4d3564d958e68053fb6652a193
thijsvl <t.vanlankveld@esciencecenter.nl>
2019-09-19 13:58:52 +0200

    Reimplemented selecting tile layers using an enum.

--- wwwroot/script/geoviz.js
+++ wwwroot/script/geoviz.js
@@ -69,14 +69,18 @@ function initMap() {
   var map = L.map('map').fitWorld();
 
   // Use CartoDB wihtout labels as base layer.
-  getTileLayer(tileServer.CARTODB_BASE).addTo(map);
+  getTileLayer({ server: tileServer.CARTODB, id: 'light_nolabels' }).addTo(map);
 
   // Add CartoDB labels as separate pane.
   map.createPane('labels');
   let paneLabels = map.getPane('labels');
   paneLabels.style.zIndex = 650; // On top of markers but below pop-ups
   paneLabels.style.pointerEvents = 'none'; // Disable mouse events on this particular pane.
-  getTileLayer(tileServer.CARTODB_LABEL, 'labels').addTo(map);
+  getTileLayer({
+    server: tileServer.CARTODB,
+    id: 'light_only_labels',
+    pane: 'labels'
+  }).addTo(map);
 
   // Add a scale control.
   L.control.scale().addTo(map);
@@ -124,25 +128,24 @@ function initMap() {
   stylesheet.insertRule('.aga-header { z-index: ' + (mapZIndexMax + 2) + ';}');
 }
 
+// Servers with a * require an id option.
 const tileServer = {
   ARCGIS: 'ArcGIS world topo',
-  CARTODB: 'CartoDB light',
-  CARTODB_BASE: 'CartoDB light (no labels)',
-  CARTODB_LABEL: 'CartoDB light (labels only)',
-  MAPBOX: 'MapBox streets v4',
-  MAPBOX_TERRAIN: 'MapBox terrain v2',
+  CARTODB: 'CartoDB light *',
+  MAPBOX: 'MapBox *',
   OSM: 'OpenStreetMap',
-  STAMEN: 'Stamen Design'
+  STAMEN: 'Stamen Design *'
 };
 
 // Add a base map from the above selection of tile servers.
 // If the pane parameter is not null, the layer is assigned to this pane.
-function getTileLayer(server = tileServer.MAPBOX, pane = null) {
+function getTileLayer(
+  options = { server: tileServer.MAPBOX, id: 'mapbox.streets' }
+) {
   let serverUrl = '';
   let tileOptions = { maxZoom: 18 };
-  if (pane !== null) tileOptions.pane = pane;
 
-  switch (server) {
+  switch (options.server) {
     case tileServer.ARCGIS:
       serverUrl =
         'http://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}';
@@ -161,27 +164,7 @@ function getTileLayer(server = tileServer.MAPBOX, pane = null) {
         'under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. ' +
         'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
         'under <a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>.';
-      tileOptions.id = 'light_all';
-      break;
-    case tileServer.CARTODB_BASE:
-      serverUrl =
-        'https://cartodb-basemaps-{s}.global.ssl.fastly.net/{id}/{z}/{x}/{y}.png';
-      tileOptions.attribution =
-        'Map tiles from <a href="http://cartodb.com/attributions">CartoDB</a>, ' +
-        'under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. ' +
-        'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-        'under <a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>.';
-      tileOptions.id = 'light_nolabels';
-      break;
-    case tileServer.CARTODB_LABEL:
-      serverUrl =
-        'https://cartodb-basemaps-{s}.global.ssl.fastly.net/{id}/{z}/{x}/{y}.png';
-      tileOptions.attribution =
-        'Map tiles from <a href="http://cartodb.com/attributions">CartoDB</a>, ' +
-        'under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. ' +
-        'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-        'under <a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>.';
-      tileOptions.id = 'light_only_labels';
+      // Expected IDs: 'light_all', 'light_nolabels', 'light_only_labels'.
       break;
     case tileServer.MAPBOX:
       serverUrl =
@@ -191,17 +174,7 @@ function getTileLayer(server = tileServer.MAPBOX, pane = null) {
         'Map data © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
         'under <a href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>. ' +
         '<strong><a href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a></strong>';
-      tileOptions.id = 'mapbox.streets';
-      break;
-    case tileServer.MAPBOX_TERRAIN:
-      serverUrl =
-        'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw';
-      tileOptions.attribution =
-        'Map tiles © <a href="https://www.mapbox.com/about/maps/">Mapbox</a>. ' +
-        'Map data © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
-        'under <a href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>. ' +
-        '<strong><a href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a></strong>';
-      tileOptions.id = 'mapbox.mapbox-terrain-v2';
+      // Expected IDs: 'mapbox.streets', 'mapbox.mapbox-terrain-v2'.
       break;
     case tileServer.OSM:
       serverUrl = 'http://{s}.tile.osm.org/{z}/{x}/{y}.png';
@@ -210,17 +183,23 @@ function getTileLayer(server = tileServer.MAPBOX, pane = null) {
         'under <a href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>.';
       break;
     case tileServer.STAMEN:
+      // Expected IDs: 'watercolor'.
       serverUrl = 'http://{s}.tile.stamen.com/{id}/{z}/{x}/{y}.jpg';
       tileOptions.attribution =
         'Map tiles © <a href="http://stamen.com">Stamen Design</a>, ' +
         'under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. ' +
         'Map data © <a href="http://openstreetmap.org">OpenStreetMap</a>, ' +
         'under <a href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>.';
-      tileOptions.id = 'watercolor';
       break;
     default:
       return null;
   }
+
+  if (options.id !== undefined && options.id !== null)
+    tileOptions.id = options.id;
+
+  if (options.pane !== undefined && options.pane !== null)
+    tileOptions.pane = options.pane;
 
   return L.tileLayer(serverUrl, tileOptions);
 }
