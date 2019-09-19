3b7115643954df8ae0aab689444f865d35143ec2
thijsvl <t.vanlankveld@esciencecenter.nl>
2019-09-19 13:43:37 +0200

    Implemented method for selecting base map from short list of servers,

--- wwwroot/script/geoviz.js
+++ wwwroot/script/geoviz.js
@@ -68,18 +68,15 @@ function tryAddSupportCard(url, cardId, gainFocus = true) {
 function initMap() {
   var map = L.map('map').fitWorld();
 
-  // Use OpenStreetMap as base layer.
-  L.tileLayer(
-    'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw',
-    {
-      maxZoom: 18,
-      attribution:
-        'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-        '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-        'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-      id: 'mapbox.streets'
-    }
-  ).addTo(map);
+  // Use CartoDB wihtout labels as base layer.
+  getTileLayer(tileServer.CARTODB_BASE).addTo(map);
+
+  // Add CartoDB labels as separate pane.
+  map.createPane('labels');
+  let paneLabels = map.getPane('labels');
+  paneLabels.style.zIndex = 650; // On top of markers but below pop-ups
+  paneLabels.style.pointerEvents = 'none'; // Disable mouse events on this particular pane.
+  getTileLayer(tileServer.CARTODB_LABEL, 'labels').addTo(map);
 
   // Add a scale control.
   L.control.scale().addTo(map);
@@ -125,4 +122,105 @@ function initMap() {
   let stylesheet = document.styleSheets[document.styleSheets.length - 1];
   stylesheet.insertRule('.aga-card { z-index: ' + (mapZIndexMax + 1) + ';}');
   stylesheet.insertRule('.aga-header { z-index: ' + (mapZIndexMax + 2) + ';}');
+}
+
+const tileServer = {
+  ARCGIS: 'ArcGIS world topo',
+  CARTODB: 'CartoDB light',
+  CARTODB_BASE: 'CartoDB light (no labels)',
+  CARTODB_LABEL: 'CartoDB light (labels only)',
+  MAPBOX: 'MapBox streets v4',
+  MAPBOX_TERRAIN: 'MapBox terrain v2',
+  OSM: 'OpenStreetMap',
+  STAMEN: 'Stamen Design'
+};
+
+// Add a base map from the above selection of tile servers.
+// If the pane parameter is not null, the layer is assigned to this pane.
+function getTileLayer(server = tileServer.MAPBOX, pane = null) {
+  let serverUrl = '';
+  let tileOptions = { maxZoom: 18 };
+  if (pane !== null) tileOptions.pane = pane;
+
+  switch (server) {
+    case tileServer.ARCGIS:
+      serverUrl =
+        'http://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}';
+      tileOptions.attribution =
+        'Map tiles © <a href="https://www.arcgis.com">ArcGIS</a> contributors. ' +
+        'Map data © <a href="http://osm.org/copyright">OpenStreetMap</a> contributors, ' +
+        'under <a href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>. ' +
+        'The GIS User Community.'; // Note that this attribution should actually be collected from the copyrightText field in http://services.arcgisonline.com/arcgis/rest/services/World_Topo_Map/MapServer?f=pjson
+      break;
+    case tileServer.CARTODB:
+      // Alternative URL: 'https://{s}.basemaps.cartocdn.com/{id}/{z}/{x}/{y}.png'
+      serverUrl =
+        'https://cartodb-basemaps-{s}.global.ssl.fastly.net/{id}/{z}/{x}/{y}.png';
+      tileOptions.attribution =
+        'Map tiles from <a href="http://cartodb.com/attributions">CartoDB</a>, ' +
+        'under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. ' +
+        'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+        'under <a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>.';
+      tileOptions.id = 'light_all';
+      break;
+    case tileServer.CARTODB_BASE:
+      serverUrl =
+        'https://cartodb-basemaps-{s}.global.ssl.fastly.net/{id}/{z}/{x}/{y}.png';
+      tileOptions.attribution =
+        'Map tiles from <a href="http://cartodb.com/attributions">CartoDB</a>, ' +
+        'under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. ' +
+        'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+        'under <a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>.';
+      tileOptions.id = 'light_nolabels';
+      break;
+    case tileServer.CARTODB_LABEL:
+      serverUrl =
+        'https://cartodb-basemaps-{s}.global.ssl.fastly.net/{id}/{z}/{x}/{y}.png';
+      tileOptions.attribution =
+        'Map tiles from <a href="http://cartodb.com/attributions">CartoDB</a>, ' +
+        'under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. ' +
+        'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+        'under <a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>.';
+      tileOptions.id = 'light_only_labels';
+      break;
+    case tileServer.MAPBOX:
+      serverUrl =
+        'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw';
+      tileOptions.attribution =
+        'Map tiles © <a href="https://www.mapbox.com/about/maps/">Mapbox</a>. ' +
+        'Map data © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
+        'under <a href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>. ' +
+        '<strong><a href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a></strong>';
+      tileOptions.id = 'mapbox.streets';
+      break;
+    case tileServer.MAPBOX_TERRAIN:
+      serverUrl =
+        'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw';
+      tileOptions.attribution =
+        'Map tiles © <a href="https://www.mapbox.com/about/maps/">Mapbox</a>. ' +
+        'Map data © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
+        'under <a href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>. ' +
+        '<strong><a href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a></strong>';
+      tileOptions.id = 'mapbox.mapbox-terrain-v2';
+      break;
+    case tileServer.OSM:
+      serverUrl = 'http://{s}.tile.osm.org/{z}/{x}/{y}.png';
+      tileOptions.attribution =
+        'Map data © <a href="http://osm.org/copyright">OpenStreetMap</a> contributors, ' +
+        'under <a href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>.';
+      break;
+    case tileServer.STAMEN:
+      serverUrl = 'http://{s}.tile.stamen.com/{id}/{z}/{x}/{y}.jpg';
+      tileOptions.attribution =
+        'Map tiles © <a href="http://stamen.com">Stamen Design</a>, ' +
+        'under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. ' +
+        'Map data © <a href="http://openstreetmap.org">OpenStreetMap</a>, ' +
+        'under <a href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>.';
+      tileOptions.id = 'watercolor';
+      break;
+    default:
+      return null;
+  }
+
+  return L.tileLayer(serverUrl, tileOptions);
 }
