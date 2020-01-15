e204aa5cd83e1f7e4398c2a80f19fd23414f1593
thijsvl <t.vanlankveld@esciencecenter.nl>
2020-01-15 14:04:28 +0100

    Renamed NecklaceShape NecklaceType, implemented NecklaceInterval, Necklace, and NecklaceGlyph and wrote documentation.

--- src/geoviz/necklace_map/necklace.cpp
+++ src/geoviz/necklace_map/necklace.cpp
@@ -22,24 +22,26 @@ Created by tvl (t.vanlankveld@esciencecenter.nl) on 07-11-2019
 
 #include "necklace.h"
 
+#include <glog/logging.h>
+
 
 namespace geoviz
 {
 namespace necklace_map
 {
 
-/**@class NecklaceType
- * @brief A necklace is a star-shaped curve that guides the placement of data visualization glyphs.
+/**@class NecklaceShape
+ * @brief A star-shaped curve that guides the placement of data visualization glyphs.
  */
 
-/**@fn virtual const Point& NecklaceType::kernel() const
+/**@fn virtual const Point& NecklaceShape::kernel() const
  * @brief Give the kernel of the necklace.
  *
  * Any ray originating from this point will intersect the necklace in at most 1 point.
  * @return the kernel of the necklace.
  */
 
-/**@fn virtual bool NecklaceType::IntersectRay(const Number& angle_rad, Point& intersection) const
+/**@fn virtual bool NecklaceShape::IntersectRay(const Number& angle_rad, Point& intersection) const
  * @brief Intersect a ray originating from the kernel with the necklace.
  * @param angle_rad the counterclockwise angle between the ray and the positive x-axis in radians.
  * @param intersection the intersection of the ray with the necklace, if it exists.
@@ -49,9 +51,14 @@ namespace necklace_map
  * @endparblock
  */
 
-/**@fn virtual Box NecklaceType::ComputeBoundingBox() const = 0
+/**@fn virtual Box NecklaceShape::ComputeBoundingBox() const = 0
  * @brief Construct a minimum bounding box of the necklace.
  * @return the minimum bounding box.
+ */
+
+/**@fn virtual Number NecklaceShape::ComputeLength() const = 0
+ * @brief Compute the total length of the necklace.
+ * @return the necklace length.
  */
 
 
@@ -66,7 +73,7 @@ namespace necklace_map
  * @param shape the circle covered by the necklace.
  */
 CircleNecklace::CircleNecklace(const Circle& shape)
-  : NecklaceType(), shape_(shape) {}
+  : NecklaceShape(), shape_(shape) {}
 
 const Point& CircleNecklace::kernel() const
 {
@@ -90,6 +97,12 @@ Box CircleNecklace::ComputeBoundingBox() const
     shape_.center().x() + radius,
     shape_.center().y() + radius
   );
+}
+
+Number CircleNecklace::ComputeLength() const
+{
+  const Number radius = CGAL::sqrt(shape_.squared_radius());
+  return M_2xPI * radius;
 }
 
 
@@ -119,6 +132,13 @@ CurveNecklace::CurveNecklace(const Circle& shape, const Number& angle_cw_rad, co
     angle_cw_rad_(angle_cw_rad),
     angle_ccw_rad_(angle_ccw_rad)
 {
+  if (angle_cw_rad_ == angle_ccw_rad_)
+  {
+    // If the necklace covers the whole circle, we set the angles to 0 for convenience.
+    angle_cw_rad_ = angle_ccw_rad_ = 0;
+    return;
+  }
+
   while (angle_cw_rad_ < 0)
     angle_cw_rad_ += M_2xPI;
   while (M_2xPI <= angle_cw_rad_)
@@ -149,6 +169,187 @@ bool CurveNecklace::IntersectRay(const Number& angle_rad, Point& intersection) c
  *
  * Note that for this necklace, the kernel must be set explicitly.
  */
+
+
+/**@class NecklaceInterval
+ * @brief A connected part of a necklace.
+ */
+
+/**@brief Construct a necklace interval.
+ *
+ * The interval covers the intersection of the necklace and a wedge with its apex at the necklace kernel. This wedge is bounded by two rays from the center, which are described by their angle relative to the positive x axis in counterclockwise direction.
+ *
+ * The order of these rays is important: the interval is used that lies counterclockwise relative to the first angle.
+ *
+ * If the rays are identical, the interval covers the full necklace. This should not occur in practice.
+ *
+ * Note that the interval includes the intersections of the wedge boundaries with the necklace, i.e. the interval is closed.
+ * @param angle_cw_rad the clockwise endpoint of the interval.
+ * @param angle_ccw_rad the counterclockwise endpoint of the interval.
+ */
+NecklaceInterval::NecklaceInterval(const Number& angle_cw_rad, const Number& angle_ccw_rad)
+  : angle_cw_rad_(angle_cw_rad), angle_ccw_rad_(angle_ccw_rad)
+{
+  if (angle_cw_rad_ == angle_ccw_rad_)
+  {
+    // If the interval covers the whole necklace, we set the angles to 0 for convenience.
+    angle_cw_rad_ = angle_ccw_rad_ = 0;
+    return;
+  }
+
+  while (angle_cw_rad_ < 0)
+    angle_cw_rad_ += M_2xPI;
+  while (M_2xPI <= angle_cw_rad_)
+    angle_cw_rad_ -= M_2xPI;
+
+  while (angle_ccw_rad_ <= angle_cw_rad_)
+    angle_ccw_rad_ += M_2xPI;
+  while (angle_cw_rad_ + M_2xPI < angle_ccw_rad_)
+    angle_ccw_rad_ -= M_2xPI;
+}
+
+/**@brief The angle where the interval ends when traversing the interval in clockwise direction.
+ *
+ * @return @parblock the clockwise ending angle.
+ *
+ * This angle is guaranteed to be in the range [0, 2*pi).
+ * @endparblock
+ */
+const Number& NecklaceInterval::angle_cw_rad() const { return angle_cw_rad_; }
+
+/**@brief The angle where the interval ends when traversing the interval in counterclockwise direction.
+ *
+ * @return @parblock the counterclockwise ending angle.
+ *
+ * This angle is guaranteed to be in the range [angle_cw_rad, angle_cw_rad+2*pi).
+ * @endparblock
+ */
+const Number& NecklaceInterval::angle_ccw_rad() const { return angle_ccw_rad_; }
+
+/**@brief Check whether the interval is in a valid state.
+ * @return whether the interval is valid.
+ */
+bool NecklaceInterval::IsValid() const
+{
+  return
+    0 <= angle_cw_rad_ && angle_cw_rad_ < M_2xPI &&
+    angle_cw_rad_ <= angle_ccw_rad_ && angle_ccw_rad_ < angle_cw_rad_ + M_2xPI;
+}
+
+/**@brief Check whether the interval intersects a ray from the necklace kernel.
+ * @param angle_rad @parblock the counterclockwise angle between the ray and the positive x axis.
+ *
+ * This angle must be in the range [0, 2*pi).
+ * @endparblock
+ * @return whether the interval contains the intersection of the ray with the necklace.
+ */
+bool NecklaceInterval::IntersectsRay(const Number& angle_rad) const
+{
+  CHECK_GE(angle_rad, 0);
+  CHECK_LT(angle_rad, M_2xPI);
+  return
+    angle_cw_rad_ == angle_ccw_rad_ ||
+    (angle_rad < angle_cw_rad_ ? angle_rad + M_2xPI : angle_rad) < angle_ccw_rad_;
+}
+
+
+/**@class IntervalCentroid
+ * @brief A centroid-based necklace interval.
+ */
+
+/**@brief Construct a centroid interval.
+ *
+ * The interval covers the intersection of the necklace and a wedge with its apex at the necklace kernel. This wedge is bounded by two rays from the center, which are described by their angle relative to the positive x axis in counterclockwise direction.
+ *
+ * The order of these rays is important: the interval is used that lies counterclockwise relative to the first angle.
+ *
+ * If the rays are identical, the interval covers the full necklace. This should not occur in practice.
+ *
+ * Note that the interval includes the intersections of the wedge boundaries with the necklace, i.e. the interval is closed.
+ * @param angle_cw_rad the clockwise endpoint of the interval.
+ * @param angle_ccw_rad the counterclockwise endpoint of the interval.
+ */
+IntervalCentroid::IntervalCentroid(const Number& angle_cw_rad, const Number& angle_ccw_rad)
+  : NecklaceInterval(angle_cw_rad, angle_ccw_rad) {}
+
+Number IntervalCentroid::ComputeOrder() const
+{
+  // Order based on centroid.
+  const Number order = (angle_cw_rad_ + angle_ccw_rad_) / 2;
+  if (M_2xPI <= order)
+    return order - M_2xPI;
+  return order;
+}
+
+
+/**@class IntervalWedge
+ * @brief A wedge-based necklace interval.
+ */
+
+/**@brief Construct a wedge interval.
+ *
+ * The interval covers the intersection of the necklace and a wedge with its apex at the necklace kernel. This wedge is bounded by two rays from the center, which are described by their angle relative to the positive x axis in counterclockwise direction.
+ *
+ * The order of these rays is important: the interval is used that lies counterclockwise relative to the first angle.
+ *
+ * If the rays are identical, the interval covers the full necklace. This should not occur in practice.
+ *
+ * Note that the interval includes the intersections of the wedge boundaries with the necklace, i.e. the interval is closed.
+ * @param angle_cw_rad the clockwise endpoint of the interval.
+ * @param angle_ccw_rad the counterclockwise endpoint of the interval.
+ */
+IntervalWedge::IntervalWedge(const Number& angle_cw_rad, const Number& angle_ccw_rad)
+  : NecklaceInterval(angle_cw_rad, angle_ccw_rad) {}
+
+Number IntervalWedge::ComputeOrder() const
+{
+  // Order based on smallest angle.
+  return angle_cw_rad_;
+}
+
+
+/**@struct Necklace
+ * @brief A collection of visualization glyphs that are organized on a curve.
+ */
+
+/**@brief Construct a necklace from a shape.
+ * @param shape the shape of the necklace.
+ */
+Necklace::Necklace(const NecklaceShape::Ptr& shape) : shape(shape)/*, glyph()*/ {}
+
+/**@fn NecklaceShape::Ptr NecklaceShape::shape
+ * @brief the shape used to organize the glyphs.
+ */
+
+/**@fn NecklaceGlyph::Ptr NecklaceShape::glyph
+ * @brief one of the glyphs on the necklace.
+ *
+ * Note that the other glyphs can be reached by traversing through glyph->next.
+ */
+
+
+/**@struct NecklaceGlyph
+ * @brief A visualization element to show the numeric value associated with a map region.
+ *
+ * Each region with a value larger than 0 that is also assigned a necklace will get a necklace glyph. The value is visualized using the area of the element. While this does not directly convey the absolute value, the difference between glyphs exposes their relative values.
+ *
+ * While glyphs could have various shapes, we currently only support disks.
+ */
+
+/**@brief Construct a necklace glyph.
+ * @param radius_base the radius of the glyph before scaling.
+ */
+NecklaceGlyph::NecklaceGlyph(const Necklace::Ptr necklace)
+  : necklace(necklace), interval(), angle_rad(0), angle_min_rad(0), angle_max_rad(0) {}
+
+/**@brief Check whether the glyph is valid.
+ *
+ * This validity depends on three aspects: the interval must not be null, the interval must be valid, and the glyph's position must be in the interval.
+ */
+bool NecklaceGlyph::IsValid() const
+{
+  return necklace != nullptr && interval != nullptr && interval->IsValid() && interval->IntersectsRay(angle_rad);
+}
 
 } // namespace necklace_map
 } // namespace geoviz
--- src/geoviz/necklace_map/necklace.h
+++ src/geoviz/necklace_map/necklace.h
@@ -23,6 +23,8 @@ Created by tvl (t.vanlankveld@esciencecenter.nl) on 07-11-2019
 #ifndef GEOVIZ_NECKLACE_MAP_NECKLACE_H
 #define GEOVIZ_NECKLACE_MAP_NECKLACE_H
 
+#include <memory>
+
 #include "geoviz/common/core_types.h"
 
 
@@ -31,28 +33,32 @@ namespace geoviz
 namespace necklace_map
 {
 
-class NecklaceType
+class NecklaceShape
 {
  public:
+  using Ptr = std::shared_ptr<NecklaceShape>;
+
   virtual const Point& kernel() const = 0;
   virtual bool IntersectRay(const Number& angle_rad, Point& intersection) const = 0;
   virtual Box ComputeBoundingBox() const = 0;
+  virtual Number ComputeLength() const = 0;
 
   // TODO(tvl) should the necklace contain methods for adapting the 1D solution to the 2D solution?
   // This would mainly come into play for Bezier necklaces. For impl: check the 30x for loop in the Java prototype code.
   // Another case would be when a Glyph contains the necklace center;
   // in this case, the scale factor should be reduced such that the glyph does not contain the necklace center
   // (could this cause recursive failure and if so, in which cases? This *may* actually prove scientifically interesting).
-}; // class NecklaceType
+}; // class NecklaceShape
 
 
-class CircleNecklace : public NecklaceType
+class CircleNecklace : public NecklaceShape
 {
  public:
   explicit CircleNecklace(const Circle& shape);
   const Point& kernel() const;
   bool IntersectRay(const Number& angle_rad, Point& intersection) const;
   Box ComputeBoundingBox() const;
+  Number ComputeLength() const;
 
  protected:
   Circle shape_;
@@ -71,17 +77,84 @@ class CurveNecklace : public CircleNecklace
 }; // class CurveNecklace
 
 
-class GenericNecklace : public NecklaceType
+class GenericNecklace : public NecklaceShape
 {
  public:
   const Point& getKernel() const;
   bool IntersectRay(const Number& angle_rad, Point& intersection) const;
   Box ComputeBoundingBox() const;
+  Number ComputeLength() const;
 
  private:
   Point kernel_;
-  std::vector<Point> points_;  // TODO(tvl) the geneic necklace will probably require different markers than just points.
+  std::vector<Point> points_;  // TODO(tvl) the generic necklace will probably require different markers than just points.
 }; // class GenericNecklace
+
+
+class NecklaceInterval
+{
+ public:
+  using Ptr = std::shared_ptr<NecklaceInterval>;
+
+  NecklaceInterval(const Number& angle_cw_rad, const Number& angle_ccw_rad);
+
+  const Number& angle_cw_rad() const;
+  const Number& angle_ccw_rad() const;
+
+  bool IsValid() const;
+
+  bool IntersectsRay(const Number& angle_rad) const;
+
+  virtual Number ComputeOrder() const = 0;
+
+ protected:
+  Number angle_cw_rad_; // Internally, this angle is adjusted to be in the range [0, 2*pi).
+  Number angle_ccw_rad_; // For convenience, this angle is adjusted to be in the range [angle_from_rad, angle_from_rad+2*pi).
+}; // class NecklaceInterval
+
+
+class IntervalCentroid : public NecklaceInterval
+{
+ public:
+  IntervalCentroid(const Number& angle_cw_rad, const Number& angle_ccw_rad);
+
+  Number ComputeOrder() const;  // Order based on centroid.
+}; // class IntervalCentroid
+
+
+class IntervalWedge : public NecklaceInterval
+{
+ public:
+  IntervalWedge(const Number& angle_cw_rad, const Number& angle_ccw_rad);
+
+  Number ComputeOrder() const;  // Order based on begin.
+}; // class IntervalWedge
+
+
+struct Necklace
+{
+  using Ptr = std::shared_ptr<Necklace>;
+
+  Necklace(const NecklaceShape::Ptr& shape);
+
+  NecklaceShape::Ptr shape;
+}; // struct Necklace
+
+
+struct NecklaceGlyph  // TODO(tvl) rename glyph => bead (including all derived types and methods).
+{
+  using Ptr = std::shared_ptr<NecklaceGlyph>;
+
+  NecklaceGlyph(const Necklace::Ptr necklace);
+
+  bool IsValid() const;
+
+  Necklace::Ptr necklace;
+  NecklaceInterval::Ptr interval;  // TODO(tvl) rename feasible.
+  Number angle_rad;
+  Number angle_min_rad;  // TODO(tvl) move out of glyph into glyph-scaler-element...
+  Number angle_max_rad;
+}; // struct NecklaceGlyph
 
 } // namespace necklace_map
 } // namespace geoviz
