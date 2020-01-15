59ee978d93a8fde3afacb39572bacea9eafbf630
thijsvl <t.vanlankveld@esciencecenter.nl>
2020-01-15 13:31:17 +0100

    Fixed bug where PHP files failed when no arguments were given.

--- wwwroot/script/template.php
+++ wwwroot/script/template.php
@@ -5,9 +5,10 @@
 <!--Note that while we could have one PHP file that takes as input the executable to run, this would pose an enormous security risk.-->
 <?php
     // Collect the command line arguments and separate them by spaces.
-    $args = $_REQUEST["args"];
+    $args = isset($_REQUEST["args"]) ? $_REQUEST["args"] : "";
     $delim = isset($_REQUEST["delim"]) ? $_REQUEST["delim"] : "|";
-    $arg_str = str_replace($delim, " ", $args);
+    $assign = isset($_REQUEST["assign"]) ? $_REQUEST["assign"] : ":";
+    $arg_str = str_replace($delim, " ", str_replace($assign, "=", $args));
     // Run the executable and return what it sent to the standard output stream.
     $result = shell_exec("../bin/executable $arg_str");
     echo $result;
