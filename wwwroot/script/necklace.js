7d98fd0c7e78621d0a180a05bc9227ddb27de59d
Willem Sonke <willem@wimiso.nl>
2020-12-07 13:45:41 +0100

    Read data files; change to CartoCrow
    This (re-)adds the functionality for the user to upload a data file,
    instead of having to manually fill in the data fields. As opposed to the
    earlier implementation, the user now has to click the Import button to
    upload the actual data file, and then the data values from it are
    imported into the data fields. This way, the user can still make
    adjustments if desired.
    
    This moves the map files to wwwroot/data so that they are accessible and
    can be requested by the JavaScript code.
    
    This commit also changes the name GeoViz to CartoCrow, and introduces
    some CartoCrow branding throughout the interface.
    
    Finally, this commit adds comments to the JavaScript methods.

--- wwwroot/script/necklace.js
+++ wwwroot/script/necklace.js
@@ -11,13 +11,18 @@ let necklace_geometry_base64 = null;
 let necklace_data_base64 = null;
 let region_focused = false;
 let column_list = null;
+let attribute = 'value';
+let svg_output = '';
 
-function setColumnList(result) {
+/**
+ * Given a data file, return a list of attribute names in it.
+ */
+function parseColumns(result) {
   let lines = result.split('\n');
   let token = 0;
   let num_col = 0;
 
-  let list = '';
+  let colNames = [];
   for (let line of lines) {
     let columns = line.split(/ +/);
 
@@ -27,30 +32,107 @@ function setColumnList(result) {
       else if (token == 2) num_col = column.length - 1;
       else {
         let col_str = escape(column.replace(/\r/, ''));
-        if (col_str != 'ID' && col_str != 'name' && col_str != 'null')
-          list += '<option value="' + col_str + '">' + col_str + '</option>';
+        colNames.push(col_str);
       }
     }
 
     if (2 + num_col <= token) break;
   }
 
+  return colNames;
+}
+
+/**
+ * Given a data file, initialize the attribute dropdown with the available
+ * attribute names. This excludes the attributes 'ID' and 'name'.
+ */
+function setColumnList(result) {
+  const columns = parseColumns(result);
+  let list = '';
+  for (let column of columns) {
+    if (column && column !== 'ID' && column !== 'name') {
+      list += '<option value="' + column + '">' + column + '</option>';
+    }
+  }
+
   document.getElementById('data_value_in').innerHTML = list;
 }
 
+/**
+ * Given a data file and the name of one of its attributes, put the data values
+ * from that attribute into the input fields.
+ *
+ * This assumes that the input fields for the regions defined in the data file
+ * already exist (see populateDataEditor()).
+ */
+function parseDataIntoFields(result, attribute) {
+  let fields = result.trim().split(/[ \n]+/);
+  const cols = parseColumns(result);
+  const colValueId = cols.indexOf(attribute);
+
+  let i = 2 + cols.length;
+  while (i < fields.length) {
+    const id = fields[i].trim();
+    const value = fields[i + colValueId];
+    document.getElementById('data-editor-editor-' + id).value = value;
+    i += cols.length;
+  }
+}
+
+/**
+ * The main initialization function.
+ */
 function initNecklaceMap() {
   focusSupportCard();
   tryAddSettingsCard('page/settings_necklace.html', 'settings_necklace');
 
   necklace_geometry_base64 = null;
   necklace_data_base64 = null;
-  if (document.getElementById('geometry_file_in') !== null)
+  if (document.getElementById('geometry_file_in') !== null) {
     geometry_file_in.value = '';
-  if (document.getElementById('data_file_in') !== null)
+  }
+  if (document.getElementById('data_file_in') !== null) {
     data_file_in.value = '';
+  }
 }
 
+/**
+ * Callback for when the user selected a different map from the dropdown.
+ *
+ * This performs an AJAX call to the server to obtain a list of regions in the
+ * selected map.
+ */
+function onChangedMap() {
+  const field = document.getElementById('map_choice');
+  const selected = field.options[field.selectedIndex].value;
+  updateFormState();
+  if (selected !== "custom") {
+
+    // download the map
+    ajaxGet('data/necklace_map/' + selected + '.svg', function (data) {
+      necklace_geometry_base64 = btoa(data);
+      necklace_data_base64 = null;
+
+      let params = JSON.stringify({
+        geometry_base64: necklace_geometry_base64,
+      });
+      ajaxPost('/script/run_map_regions.php', params, populateDataEditor);
+      region_focused = false;
+
+      updateFormState();
+    });
+  }
+}
+
+/**
+ * Callback for when the user selected a different map with the custom map
+ * control.
+ *
+ * This performs an AJAX call to the server to obtain a list of regions in the
+ * selected map.
+ */
 function onChangedGeometryFile(file) {
+
   if (!file) {
     return;
   }
@@ -80,12 +162,17 @@ function onChangedGeometryFile(file) {
       geometry_base64: necklace_geometry_base64,
     });
     ajaxPost('/script/run_map_regions.php', params, populateDataEditor);
+    region_focused = false;
 
     updateFormState();
   };
   reader.readAsArrayBuffer(file);
 }
 
+/**
+ * Given a newline-separated list of regions, this initializes the data editor
+ * by creating an input field for each region.
+ */
 function populateDataEditor(response) {
   const regions = response.trim().split('\n');
 
@@ -109,10 +196,25 @@ function populateDataEditor(response) {
   }
 }
 
+/**
+ * Ensures that the correct parts of the form are hidden or made visible,
+ * depending on where the user is in the process.
+ *
+ * More precisely:
+ *
+ *  - The custom map control is shown iff the custom map option is selected
+ *    in the map dropdown.
+ *
+ *  - If no map has been specified, the remaining parts of the form are hidden.
+ *    Otherwise, the algorithm is run.
+ *
+ *  - If no data has been specified, the remaining parts of the form are
+ *    hidden.
+ */
 function updateFormState() {
 
   // if the custom map option has been set, show the file picker
-  const field = document.getElementById('geometry_choice');
+  const field = document.getElementById('map_choice');
   const selected = field.options[field.selectedIndex].value;
   if (selected === "custom") {
     document.getElementById('geometry_file_in').style.display = 'inline-block';
@@ -138,7 +240,45 @@ function updateFormState() {
   document.getElementById('options_panel').style.display = 'block';
 }
 
-function onChangedDataFile(file) {
+/**
+ * Callback for when the user selected a data file in the file picker. This
+ * reads the file to be able to initialize the attributes list.
+ */
+function onChangedDataFile() {
+  const fileField = document.getElementById('data_file_in');
+  const file = fileField.files[0];
+
+  if (!file) {
+    return;
+  }
+
+  // check mime type and file size
+  if (file.type != 'text/plain') {
+    alert('Data files need to be plain text files');
+    return;
+  }
+  if (MAX_FILE_SIZE_BYTES < file.size) {
+    alert('Data file needs to be at most ' + MAX_FILE_SIZE_BYTES + 'bytes');
+    return;
+  }
+
+  // read the file
+  var reader = new FileReader();
+  reader.onload = function (e) {
+    let result = String.fromCharCode(...new Uint8Array(e.target.result));
+    setColumnList(result);
+  };
+  reader.readAsArrayBuffer(file);
+}
+
+/**
+ * Callback for when the user clicks the Import button. This reads the file
+ * and puts the values into the input fields.
+ */
+function onImportButtonClicked() {
+  const fileField = document.getElementById('data_file_in');
+  const file = fileField.files[0];
+
   if (!file) {
     return;
   }
@@ -158,12 +298,17 @@ function onChangedDataFile(file) {
   reader.onload = function (e) {
     let result = String.fromCharCode(...new Uint8Array(e.target.result));
     necklace_data_base64 = btoa(result);
-    setColumnList(result);
+    attribute = document.getElementById('data_value_in').value;
+    parseDataIntoFields(result, attribute);
     updateFormState();
   };
   reader.readAsArrayBuffer(file);
 }
 
+/**
+ * Callback for when the user clicks the Save data values button. This reads
+ * the contents of the input fields, and generates a data file based on that.
+ */
 function onSaveButtonClicked() {
   const editor = document.getElementById('data-editor');
   let data = editor.childNodes.length + " sd\nID value\n";
@@ -177,6 +322,7 @@ function onSaveButtonClicked() {
     data += id + " " + value + "\n";
   }
   necklace_data_base64 = btoa(data);
+  attribute = "value";
 
   updateFormState();
 }
@@ -206,17 +352,39 @@ function getNecklaceBeadIdSize() {
   return size.toPrecision(4);
 }
 
+/**
+ * Generates a function replacing the Leaflet map by the output from the
+ * algorithm.
+ */
 function processNecklaceMapResponse() {
   return function (response) {
     replaceMapBySvgResponse(!region_focused)(response);
     region_focused = true;
-    //geometry_out.value = response;
-    document.getElementById('output_panel').style.display = 'block';
+    svg_output = response;
   };
 }
 
+/**
+ * Callback for when the user changed any settings.
+ *
+ * This handles placing the setting values in the output fields, and updating
+ * the explanation for the interval setting.
+ *
+ * Afterwards, it reruns the algorithm with the updated settings.
+ */
 function onChangedNecklaceSettings() {
   if (necklace_geometry_base64 === null) return;
+
+  let buffer_rad = getNecklaceBuffer();
+  let glyph_aversion = getNecklaceAversion();
+  let centroid_interval_length = getNecklaceCentroidIntervalLength();
+  let wedge_interval_length_min = getNecklaceWedgeIntervalLengthMin();
+  let bead_id_font_size = getNecklaceBeadIdSize();
+  buffer_rad_out.value = '= ' + buffer_rad;
+  aversion_out.value = '= ' + glyph_aversion;
+  centroid_interval_length_out.value = '= ' + centroid_interval_length;
+  wedge_interval_length_min_out.value = '= ' + wedge_interval_length_min;
+  bead_id_font_size_out.value = '= ' + parseInt(bead_id_font_size);
 
   if (document.getElementById('interval_in').value === "centroid") {
     document.getElementById('interval-explanation-image').src = 'res/bead-placement-centroid.svg';
@@ -231,17 +399,18 @@ function onChangedNecklaceSettings() {
   runNecklaceAlgorithm();
 }
 
+/**
+ * Performs an AJAX request to the server to run the actual algorithm.
+ *
+ * If the data has not been specified yet, no AJAX call is made, and instead
+ * the input SVG map is used as output.
+ */
 function runNecklaceAlgorithm() {
 
   if (!necklace_data_base64) {
     let necklace_geometry = atob(necklace_geometry_base64);
     processNecklaceMapResponse()(necklace_geometry);
-
-    // The geometry has not been processed yet.
-    //geometry_out.value = '';
-    document.getElementById('output_panel').style.display = 'none';
     setColumnList('');
-
     return;
   }
 
@@ -252,7 +421,7 @@ function runNecklaceAlgorithm() {
   let body = JSON.stringify({
     geometry_base64: necklace_geometry_base64,
     data_base64: necklace_data_base64,
-    value: "value",
+    value: attribute,
     interval: document.getElementById('interval_in').value,
     ignore_point_regions: ignore_point_regions_in.checked,
     order: order_in.value,
@@ -263,25 +432,7 @@ function runNecklaceAlgorithm() {
     bead_id_font_size: parseFloat(getNecklaceBeadIdSize()),
   });
 
-  //geometry_out.value = '';
-  document.getElementById('output_panel').style.display = 'none';
-
   // Run the necklace map PHP script on the server.
   ajaxPost('/script/run_necklace_map.php', body, processNecklaceMapResponse());
-}
-
-function onInputNecklaceSettings() {
-  // TODO refactor this into the methods above
-  let buffer_rad = getNecklaceBuffer();
-  let glyph_aversion = getNecklaceAversion();
-  let centroid_interval_length = getNecklaceCentroidIntervalLength();
-  let wedge_interval_length_min = getNecklaceWedgeIntervalLengthMin();
-  let bead_id_font_size = getNecklaceBeadIdSize();
-  buffer_rad_out.value = '= ' + buffer_rad;
-  aversion_out.value = '= ' + glyph_aversion;
-  centroid_interval_length_out.value = '= ' + centroid_interval_length;
-  wedge_interval_length_min_out.value = '= ' + wedge_interval_length_min;
-  bead_id_font_size_out.value = '= ' + parseInt(bead_id_font_size);
-  onChangedNecklaceSettings();
 }
 
