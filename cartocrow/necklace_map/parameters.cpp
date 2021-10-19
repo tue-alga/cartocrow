b1a8b9c9e68873658b3fe725326ffad49f52cb09
Willem Sonke <willem@wimiso.nl>
2021-10-19 16:13:00 +0200

    Fix documentation for necklace map parameters

--- cartocrow/necklace_map/parameters.cpp
+++ cartocrow/necklace_map/parameters.cpp
@@ -25,90 +25,10 @@ Created by tvl (t.vanlankveld@esciencecenter.nl) on 28-01-2020
 namespace cartocrow {
 namespace necklace_map {
 
-/**@class cartocrow::necklace_map::IntervalType
- * @brief A type of feasible interval on a necklace.
- */
-
-/**@class cartocrow::necklace_map::OrderType
- * @brief A type of ordering to apply when computing the optimal scale factor and bead placement.
- */
-
-/**@struct Parameters
- * @brief A struct to collect the parameters used for computing the necklace map.
- *
- * These parameters include those needed for computing the feasible intervals, the optimal scale factor, and a valid placement for the necklace beads.
- */
-
-/**@brief Construct a collection of parameters;
- *
- * All parameters are initialized as valid values.
- */
 Parameters::Parameters()
     : interval_type(IntervalType::kCentroid), centroid_interval_length_rad(1),
       ignore_point_regions(false), order_type(OrderType::kFixed), buffer_rad(0),
       binary_search_depth(10), heuristic_cycles(5), placement_cycles(30), aversion_ratio(0) {}
-
-/**@fn IntervalType Parameters::interval_type;
- * @brief The type of feasible intervals to compute.
- */
-
-/**@fn Number Parameters::centroid_interval_length_rad;
- * @brief The length of any centroid intervals generated when computing the feasible intervals.
- */
-
-/**@fn Number Parameters::wedge_interval_length_min_rad;
- * @brief The minimum length of any wedge interval generated when computing the feasible intervals.
- *
- * If a generated wedge interval is shorter than this length, it is replaced by a centroid interval with this length.
- */
-
-/**@fn bool Parameters::ignore_point_regions;
- * @brief Whether to ignore degenerate (point) regions.
- *
- * Point regions that are not ignored are always assigned a centroid region.
- */
-
-/**@fn OrderType Parameters::order_type;
- * @brief The type of order imposed on the necklace beads.
- *
- * This order is used when computing the optimal scale factor and when computing a valid placement.
- */
-
-/**@fn Number Parameters::buffer_rad;
- * @brief The minimum angle in radians of the empty wedge between neighboring necklace beads that has the necklace kernel as apex.
- *
- * This buffer is used when computing the optimal scale factor and when computing a valid placement.
- */
-
-/**@fn Number Parameters::binary_search_depth;
- * @brief The depth of the binary search tree used for the any-order decision problem.
- *
- * A larger depth will produce higher precision at the cost of processing time.
- */
-
-/**@fn Number Parameters::heuristic_cycles;
- * @brief The number of steps for the heuristic any-order scale factor computation.
- *
- * If the number of steps is 0, the exact algorithm is used. Otherwise, a larger number of steps results in a higher probability of generating the correct outcome of the any-order scale computation decision problem.
- */
-
-/**@fn Number Parameters::heuristic_cycles;
- * @brief The number of steps for applying the placement forces.
- *
- * A larger number of steps results in a more accurate placement.
- */
-
-/**@fn Number Parameters::placement_cycles;
- * @brief The number of steps for the placement heuristic.
- *
- * Must be non-negative. If the number of cycles is 0, all beads are placed in the most clockwise valid position.
- */
-
-/**@fn Number Parameters::aversion_ratio;
- * @brief The ratio between attraction to the interval center (0) and repulsion from the neighboring beads (1).
- *
- * This ratio must be in the range (0, 1].
- */
 
 } // namespace necklace_map
 } // namespace cartocrow
--- cartocrow/necklace_map/parameters.h
+++ cartocrow/necklace_map/parameters.h
@@ -28,28 +28,62 @@ Created by tvl (t.vanlankveld@esciencecenter.nl) on 28-01-2020
 namespace cartocrow {
 namespace necklace_map {
 
+/// A type of feasible interval on a necklace.
 enum class IntervalType { kCentroid, kWedge };
 
+/// A type of ordering to apply when computing the optimal scale factor and bead placement.
 enum class OrderType { kFixed, kAny };
 
+/// A struct to collect the parameters used for computing the necklace map.
+/**
+ * These parameters include those needed for computing the feasible intervals,
+ * the optimal scale factor, and a valid placement for the necklace beads.
+ */
 struct Parameters {
+	/// Construct a collection of parameters.
+	/// All parameters are initialized as valid values.
 	Parameters();
 
-	// Feasible interval.
+	/// The type of feasible intervals to compute.
 	IntervalType interval_type;
+	/// The length of any centroid intervals generated when computing the
+	/// feasible intervals.
 	Number centroid_interval_length_rad;
+	/// The minimum length of any wedge interval generated when computing the
+	/// feasible intervals.
+	/// If a generated wedge interval is shorter than this length, it is
+	/// replaced by a centroid interval with this length.
 	Number wedge_interval_length_min_rad;
+	/// Whether to ignore degenerate (point) regions.
+	/// Non-ignored point regions are always assigned a centroid region.
 	bool ignore_point_regions;
-
-	// Scale factor.
+	/// The type of order imposed on the necklace beads.
+	/// This order is used when computing the optimal scale factor and when
+	/// computing a valid placement.
 	OrderType order_type;
+	/// The minimum angle in radians of the empty wedge between neighboring
+	/// necklace beads that has the necklace kernel as apex.
+	/// This buffer is used when computing the optimal scale factor and when
+	/// computing a valid placement.
 	Number buffer_rad;
-
+	/// The depth of the binary search tree used for the any-order decision
+	/// problem.
+	/// A larger depth will produce higher precision at the cost of processing
+	/// time.
 	int binary_search_depth;
+	/// The number of steps for the heuristic any-order scale factor
+	/// computation.
+	/// If the number of steps is 0, the exact algorithm is used. Otherwise, a
+	/// larger number of steps results in a higher probability of generating the
+	/// correct outcome of the any-order scale computation decision problem.
 	int heuristic_cycles;
-
-	// Placement.
+	/// The number of steps for the placement heuristic.
+	/// Must be non-negative. If the number of cycles is 0, all beads are placed
+	/// in the most clockwise valid position.
 	int placement_cycles;
+	/// The ratio between attraction to the interval center (0) and repulsion
+	/// from the neighboring beads (1).
+	/// This ratio must be in the range (0, 1].
 	Number aversion_ratio;
 }; // struct Parameters
 
