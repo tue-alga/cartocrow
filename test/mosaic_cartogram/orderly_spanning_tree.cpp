bf3ccd297799503e1d931c29dd60c8082b15d8f0
Gijs Pennings <49961459+gijs-pennings@users.noreply.github.com>
2023-03-23 13:18:47 +0100

    Simplify (future) test case

--- test/mosaic_cartogram/orderly_spanning_tree.cpp
+++ test/mosaic_cartogram/orderly_spanning_tree.cpp
@@ -1,48 +1,20 @@
 // TODO: add test case that tree is indeed orderly & spanning
 
 /*
-Graph g(12);
+UndirectedGraph g(12);
 
-g.addEdge(0, 1);
-g.addEdge(0, 2);
-g.addEdge(0, 5);
-g.addEdge(0, 6);
-g.addEdge(0, 11);
+g.setAdjacenciesUnsafe( 0, { 1, 2, 5, 6, 11 });
+g.setAdjacenciesUnsafe( 1, { 0, 11, 9, 4, 3, 2 });
+g.setAdjacenciesUnsafe( 2, { 0, 1, 3, 4, 8, 5 });
+g.setAdjacenciesUnsafe( 3, { 1, 4, 2 });
+g.setAdjacenciesUnsafe( 4, { 1, 9, 8, 2, 3 });
+g.setAdjacenciesUnsafe( 5, { 0, 2, 8, 7, 6 });
+g.setAdjacenciesUnsafe( 6, { 0, 5, 7, 11 });
+g.setAdjacenciesUnsafe( 7, { 5, 8, 9, 10, 11, 6 });
+g.setAdjacenciesUnsafe( 8, { 2, 4, 9, 7, 5 });
+g.setAdjacenciesUnsafe( 9, { 1, 11, 10, 7, 8, 4 });
+g.setAdjacenciesUnsafe(10, { 7, 9, 11 });
+g.setAdjacenciesUnsafe(11, { 0, 6, 7, 10, 9, 1 });
 
-g.addEdge(1, 2);
-g.addEdge(1, 3);
-g.addEdge(1, 4);
-g.addEdge(1, 9);
-g.addEdge(1, 11);
-
-g.addEdge(2, 3);
-g.addEdge(2, 4);
-g.addEdge(2, 5);
-g.addEdge(2, 8);
-
-g.addEdge(3, 4);
-
-g.addEdge(4, 8);
-g.addEdge(4, 9);
-
-g.addEdge(5, 6);
-g.addEdge(5, 7);
-g.addEdge(5, 8);
-
-g.addEdge(6, 7);
-g.addEdge(6, 11);
-
-g.addEdge(7, 8);
-g.addEdge(7, 9);
-g.addEdge(7, 10);
-g.addEdge(7, 11);
-
-g.addEdge(8, 9);
-
-g.addEdge(9, 10);
-g.addEdge(9, 11);
-
-g.addEdge(10, 11);
-
-OrderlySpanningTree ost(g, 0, 1, 11);
+detail::OrderlySpanningTree ost(g, 0, 1, 11);
 */
