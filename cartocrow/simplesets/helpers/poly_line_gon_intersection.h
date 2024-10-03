ee81e1970d1d7418570c1e1b6a3a79305a497bed
Yvee1 <stevenvdb@live.nl>
2024-10-03 15:24:39 +0200

    Work around CGAL bug

--- cartocrow/simplesets/helpers/poly_line_gon_intersection.h
+++ cartocrow/simplesets/helpers/poly_line_gon_intersection.h
@@ -5,6 +5,8 @@
 #include "cs_polyline_helpers.h"
 #include "cs_polygon_helpers.h"
 #include <CGAL/Arrangement_with_history_2.h>
+#include <CGAL/Arr_observer.h>
+#include <CGAL/Arr_extended_dcel.h>
 
 namespace cartocrow::simplesets {
 // todo: edge case where edges of dilated patterns overlap, so a half-edge may have multiple origins.
@@ -15,32 +17,173 @@ std::vector<CSPolyline> difference(const CSPolyline& line, const CSPolygonWithHo
 
 template <class OutputIterator>
 void intersection(const CSPolyline& line, const CSPolygonWithHoles& gon, OutputIterator out, bool difference, bool keepOverlap) {
+	CSTraits traits;
+	auto equals = traits.equal_2_object();
+	enum Origin {
+		Polyline,
+		PolygonOuter,
+		PolygonHole,
+	};
+	struct HalfEdgeData {
+		std::vector<Origin> origins;
+	};
+	using Arr = CGAL::Arrangement_2<CSTraits, CGAL::Arr_extended_dcel<CSTraits, std::monostate, HalfEdgeData, std::monostate>>;
+	using FH = typename Arr::Face_handle;
+	using HeH = typename Arr::Halfedge_handle;
+	using VH = typename Arr::Vertex_handle;
+
+	Arr arr;
+	class Observer : public CGAL::Arr_observer<Arr> {
+	  public:
+		Observer(Arr& arr) : CGAL::Arr_observer<Arr>(arr) {}
+
+		virtual void after_create_edge(HeH e) {
+			e->data().origins.push_back(currentOrigin);
+			e->twin()->data().origins.push_back(currentOrigin);
+		}
+
+		virtual void before_split_edge(HeH e, VH v, const X_monotone_curve_2& c1, const X_monotone_curve_2& c2) {
+			beforeSplitData = e->data();
+		}
+
+		virtual void after_split_edge(HeH e1, HeH e2) {
+			e1->set_data(beforeSplitData);
+			e1->twin()->set_data(beforeSplitData);
+			e2->twin()->set_data(beforeSplitData);
+			e2->set_data(beforeSplitData);
+		}
+
+		virtual void after_modify_edge(HeH e) {
+			e->data().origins.push_back(currentOrigin);
+			e->twin()->data().origins.push_back(currentOrigin);
+		}
+
+		Origin currentOrigin;
+
+	  private:
+		HalfEdgeData beforeSplitData;
+	};
+
+	Observer observer(arr);
+
+	observer.currentOrigin = Origin::Polyline;
+	for (auto cit = line.curves_begin(); cit != line.curves_end(); ++cit) {
+		CGAL::insert(arr, *cit);
+	}
+	observer.currentOrigin = Origin::PolygonOuter;
+	for (auto cit = gon.outer_boundary().curves_begin(); cit != gon.outer_boundary().curves_end(); ++cit) {
+		CGAL::insert(arr, *cit);
+	}
+	observer.currentOrigin = Origin::PolygonHole;
+	for (auto hit = gon.holes_begin(); hit != gon.holes_end(); ++hit) {
+		for (auto cit = hit->curves_begin(); cit != hit->curves_end(); ++cit) {
+			CGAL::insert(arr, *cit);
+		}
+	}
+
+	std::vector<typename Arr::Halfedge_handle> line_edges_keep;
+	for (auto eit = arr.edges_begin(); eit != arr.edges_end(); ++eit) {
+		HeH edge = eit;
+
+		bool onGonEdge = false;
+		bool onPolyline = false;
+		for (auto origin : edge->data().origins) {
+			if (origin == Origin::Polyline) {
+				onPolyline = true;
+			}
+			if (origin == Origin::PolygonOuter || origin == Origin::PolygonHole) {
+				onGonEdge = true;
+			}
+		}
+		if (!onPolyline) {
+			continue;
+		}
+		bool liesInGon = false;
+		if (onGonEdge) {
+			if (keepOverlap) {
+				liesInGon = true;
+			}
+		} else {
+			for (auto fh : {edge->face(), edge->twin()->face()}) {
+				if (!fh->has_outer_ccb()) {
+					continue;
+				}
+				auto ccb = fh->outer_ccb();
+				auto ccbIt = ccb;
+				do {
+					if (!equals(ccbIt->source()->point(), ccbIt->curve().source()))
+						continue;
+					// if *ccbIt lies on outer face.
+					for (auto origin : ccbIt->data().origins) {
+						if (origin == Origin::PolygonOuter) {
+							liesInGon = true;
+							break;
+						}
+					}
+				} while (++ccbIt != ccb);
+				if (liesInGon)
+					break;
+			}
+		}
+
+		if (!difference && liesInGon) {
+			if (equals(edge->source()->point(), edge->curve().source())) {
+				line_edges_keep.push_back(edge);
+			} else {
+				line_edges_keep.push_back(edge->twin());
+			}
+		}
+		if (difference && !liesInGon) {
+			if (equals(edge->source()->point(), edge->curve().source())) {
+				line_edges_keep.push_back(edge);
+			} else {
+				line_edges_keep.push_back(edge->twin());
+			}
+		}
+	}
+
+	auto originatesFromPolyline = [&arr, &equals](HeH h) {
+		for (auto origin : h->data().origins) {
+			if (origin == Origin::Polyline && equals(h->source()->point(), h->curve().source())) {
+				return true;
+			}
+		}
+		return false;
+	};
+
+	while (!line_edges_keep.empty()) {
+		// Find first edge on connected component of polyline (in the intersection with polygon)
+		auto start = line_edges_keep.front();
+		auto curr = start;
+		while (originatesFromPolyline(curr->prev())) {
+			curr = curr->prev();
+
+			if (curr == start) {
+				break;
+			}
+		}
+		std::vector<X_monotone_curve_2> xmcs;
+		auto last_it = line_edges_keep.end();
+		do {
+			last_it = std::remove(line_edges_keep.begin(), last_it, curr);
+			xmcs.push_back(curr->curve());
+			curr = curr->next();
+		} while (originatesFromPolyline(curr));
+		line_edges_keep.erase(last_it, line_edges_keep.end());
+
+		++out = CSPolyline(xmcs.begin(), xmcs.end());
+	}
+}
+
+// May crash due to CGAL bug: https://github.com/CGAL/cgal/issues/8468
+template <class OutputIterator>
+void intersectionCrashes(const CSPolyline& line, const CSPolygonWithHoles& gon, OutputIterator out, bool difference, bool keepOverlap) {
 	PolyCSTraits traits;
 	auto equals = traits.equal_2_object();
 	using Arr = CGAL::Arrangement_with_history_2<PolyCSTraits>;
 	Arr arr;
 	auto linePolycurve = arrPolycurveFromCSPolyline(line);
 	auto outerGonPolycurve = arrPolycurveFromCSPolygon(gon.outer_boundary());
-
-//	std::cout << "!" << std::endl;
-//	for (auto cit = outerGonPolycurve.subcurves_begin(); cit != outerGonPolycurve.subcurves_end(); ++cit) {
-//		if (cit->is_circular()) {
-//			std::cout << cit->supporting_circle().center() << " " << cit->supporting_circle().squared_radius() << std::endl;
-//		}
-//		if (cit->is_linear()) {
-//			std::cout << cit->supporting_line() << std::endl;
-//		}
-//		std::cout << cit->source() << " -> " << cit->target() << std::endl;
-//	}
-//	for (auto cit = linePolycurve.subcurves_begin(); cit != linePolycurve.subcurves_end(); ++cit) {
-//		if (cit->is_circular()) {
-//			std::cout << cit->supporting_circle().center() << " " << cit->supporting_circle().squared_radius() << std::endl;
-//		}
-//		if (cit->is_linear()) {
-//			std::cout << cit->supporting_line() << std::endl;
-//		}
-//		std::cout << cit->source() << " -> " << cit->target() << std::endl;
-//	}
 
 	auto ogch = CGAL::insert(arr, outerGonPolycurve);
 	auto lch = CGAL::insert(arr, linePolycurve);
--- test/simplesets/poly_line_gon_intersection.cpp
+++ test/simplesets/poly_line_gon_intersection.cpp
@@ -20,6 +20,20 @@ TEST_CASE("Intersection lies in polygon and has correct orientation") {
 	CHECK(result[0].curves_begin()->target() == OneRootPoint(1, 0));
 }
 
+TEST_CASE("Boundary overlap") {
+	X_monotone_curve_2 xm_curve({-2, 0}, {2, 0});
+	std::vector xm_curves{xm_curve};
+	CSPolyline polyline(xm_curves.begin(), xm_curves.end());
+	std::vector<X_monotone_curve_2> xm_curves_pgn{{{-4, 0}, {4, 0}}, {{4, 0}, {4, 2}}, {{4, 2}, {-4, 2}}, {{-4, 2}, {-4, 0}}};
+	CSPolygon polygon(xm_curves_pgn.begin(), xm_curves_pgn.end());
+	auto result1 = intersection(polyline, polygon, false);
+	auto result2 = intersection(polyline, polygon, true);
+	CHECK(result1.empty());
+	CHECK(!result2.empty());
+	CHECK(result2[0].curves_begin()->source() == OneRootPoint(-2, 0));
+	CHECK(result2[0].curves_begin()->target() == OneRootPoint(2, 0));
+}
+
 TEST_CASE("Multiple and connected parts of intersection") {
 	Number<Exact> half = 1;
 	half /= 2;
@@ -44,67 +58,60 @@ TEST_CASE("Multiple and connected parts of intersection") {
 	CHECK(r2->curves_begin()->target() == OneRootPoint(half, 0));
 }
 
-//TEST_CASE("Crashes") {
-//	auto r = 5.204 * 3;
-//	auto r_ = 5.204 * 3 * 0.675;
-//	auto r2 = r * r;
-//	auto r2_ = r_ * r_;
-//	Circle<Exact> c1({2597.9, -364.3}, r2, CGAL::CLOCKWISE);
-//	Circle<Exact> c2({2609.2, -342.6}, r2, CGAL::CLOCKWISE);
-//	Circle<Exact> c2_({2609.2, -342.6}, r2_, CGAL::CLOCKWISE);
-//
-//	auto getIntersections = [](const Circle<Exact>& one, const Circle<Exact>& two) {
-//	 CSArrangement arr;
-//	 CGAL::insert(arr, one);
-//	 CGAL::insert(arr, two);
-//	 std::vector<OneRootPoint> intersectionPoints;
-//	 for (auto vit = arr.vertices_begin(); vit != arr.vertices_end(); ++vit) {
-//	 	if (vit->degree() == 4) {
-//	 		intersectionPoints.push_back(vit->point());
-//	 	}
-//	 }
-//	 std::sort(intersectionPoints.begin(), intersectionPoints.end(), [](const OneRootPoint& p1, const OneRootPoint& p2) { return p1.x() < p2.x(); });
-//	 assert(intersectionPoints.size() == 2);
-//	 return intersectionPoints;
-//	};
-//
-//	auto isp12 = getIntersections(c1, c2);
-//
-//	Curve_2 arc1(c1, isp12[0], isp12[1]);
-//	Curve_2 arc2(c2, isp12[1], isp12[0]);
-//	std::vector<Curve_2> pgnArcs({arc1, arc2});
-//
-//	PolyCSTraits traits;
-//	auto construct = traits.construct_curve_2_object();
-//	CSPolycurve pgnCurve(pgnArcs.begin(), pgnArcs.end());
-//
-//	CSArrangement arr;
-//	CGAL::insert(arr, c1);
-//	CGAL::insert(arr, c2);
-//	CGAL::insert(arr, c2_);
-//
-//	CSArrangement::Face_handle fh;
-//	for (auto eit = arr.halfedges_begin(); eit != arr.halfedges_end(); ++eit) {
-//		if (eit->source()->point() == isp12[0]) {
-//			fh = eit->face();
-//		}
-//	}
-//
-//	std::vector<X_monotone_curve_2> xm_curves;
-//	curvesToXMonotoneCurves(pgnArcs.begin(), pgnArcs.end(), std::back_inserter(xm_curves));
-//	CSPolygon pgn(xm_curves.begin(), xm_curves.end());
-//
-//	auto plnPoly = ccb_to_polygon<CSTraits>(fh->outer_ccb());
-//	CSPolyline pln(plnPoly.curves_begin(), plnPoly.curves_end());
-//	intersection(pln, pgn, true);
-//
-//	IpeRenderer ipeRenderer;
-//	ipeRenderer.addPainting([&pgn, &pln](GeometryRenderer& renderer) {
-//		auto path = renderPath(pgn);
-//		renderer.draw(path);
-//	  	renderer.draw(renderPath(pln));
-//	});
-//	ipeRenderer.save("crash.ipe");
-//}
+// Test case in https://github.com/CGAL/cgal/issues/8468
+TEST_CASE("Poly-circular-arcs that partially overlap") {
+	auto r = 5.204 * 3;
+	auto r_ = 5.204 * 3 * 0.675;
+	auto r2 = r * r;
+	auto r2_ = r_ * r_;
+	Circle<Exact> c1({2597.9, -364.3}, r2, CGAL::CLOCKWISE);
+	Circle<Exact> c2({2609.2, -342.6}, r2, CGAL::CLOCKWISE);
+	Circle<Exact> c2_({2609.2, -342.6}, r2_, CGAL::CLOCKWISE);
 
-// todo: test difference, overlapping edges, and holes
+	auto getIntersections = [](const Circle<Exact>& one, const Circle<Exact>& two) {
+	 CSArrangement arr;
+	 CGAL::insert(arr, one);
+	 CGAL::insert(arr, two);
+	 std::vector<OneRootPoint> intersectionPoints;
+	 for (auto vit = arr.vertices_begin(); vit != arr.vertices_end(); ++vit) {
+	 	if (vit->degree() == 4) {
+	 		intersectionPoints.push_back(vit->point());
+	 	}
+	 }
+	 std::sort(intersectionPoints.begin(), intersectionPoints.end(), [](const OneRootPoint& p1, const OneRootPoint& p2) { return p1.x() < p2.x(); });
+	 assert(intersectionPoints.size() == 2);
+	 return intersectionPoints;
+	};
+
+	auto isp12 = getIntersections(c1, c2);
+
+	Curve_2 arc1(c1, isp12[0], isp12[1]);
+	Curve_2 arc2(c2, isp12[1], isp12[0]);
+	std::vector<Curve_2> pgnArcs({arc1, arc2});
+
+	PolyCSTraits traits;
+	auto construct = traits.construct_curve_2_object();
+	CSPolycurve pgnCurve(pgnArcs.begin(), pgnArcs.end());
+
+	CSArrangement arr;
+	CGAL::insert(arr, c1);
+	CGAL::insert(arr, c2);
+	CGAL::insert(arr, c2_);
+
+	CSArrangement::Face_handle fh;
+	for (auto eit = arr.halfedges_begin(); eit != arr.halfedges_end(); ++eit) {
+		if (eit->source()->point() == isp12[0]) {
+			fh = eit->face();
+		}
+	}
+
+	std::vector<X_monotone_curve_2> xm_curves;
+	curvesToXMonotoneCurves(pgnArcs.begin(), pgnArcs.end(), std::back_inserter(xm_curves));
+	CSPolygon pgn(xm_curves.begin(), xm_curves.end());
+
+	auto plnPoly = ccb_to_polygon<CSTraits>(fh->outer_ccb());
+	CSPolyline pln(plnPoly.curves_begin(), plnPoly.curves_end());
+	intersection(pln, pgn, true);
+}
+
+// todo: test difference and holes
