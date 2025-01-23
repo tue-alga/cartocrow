13f9565d790f13c8915dee093d9d0be7eb9506fe
Yvee1 <stevenvdb@live.nl>
2025-01-23 10:27:15 +0100

    Add SimpleSets frontend

--- frontend/cartocrow.cpp
+++ frontend/cartocrow.cpp
@@ -36,6 +36,10 @@ Created by tvl (t.vanlankveld@esciencecenter.nl) on 10-09-2019
 #include "cartocrow/isoline_simplification/ipe_isolines.h"
 #include "cartocrow/isoline_simplification/isoline_simplifier.h"
 #include "cartocrow/isoline_simplification/simple_isoline_painting.h"
+#include "cartocrow/simplesets/parse_input.h"
+#include "cartocrow/simplesets/settings.h"
+#include "cartocrow/simplesets/partition_algorithm.h"
+#include "cartocrow/simplesets/drawing_algorithm.h"
 #include "cartocrow/necklace_map/circle_necklace.h"
 #include "cartocrow/necklace_map/necklace_map.h"
 #include "cartocrow/necklace_map/painting.h"
@@ -122,6 +126,88 @@ int main(int argc, char* argv[]) {
 		int target = projectData["target"];
 		simplifier.simplify(target);
 		painting = std::make_shared<isoline_simplification::SimpleIsolinePainting>(simplifier.m_simplified_isolines);
+	} else if (projectData["type"] == "simplesets") {
+		// Parse points
+		auto filePath = projectFilename.parent_path() / projectData["points"];
+		std::ifstream inputStream(filePath, std::ios_base::in);
+		if (!inputStream.good()) {
+			throw std::runtime_error("Failed to read input");
+		}
+		std::stringstream buffer;
+		buffer << inputStream.rdbuf();
+		auto points = simplesets::parseCatPoints(buffer.str());
+
+		// Parse settings
+		const auto& pd = projectData;
+
+		simplesets::GeneralSettings gs;
+		const auto& pdgs = pd["generalSettings"];
+		gs.pointSize = pdgs["pointSize"];
+		gs.inflectionLimit = pdgs["inflectionLimit"];
+		gs.maxBendAngle = pdgs["maxBendAngle"];
+		gs.maxTurnAngle = pdgs["maxTurnAngle"];
+
+		simplesets::DrawSettings ds;
+		const auto& pdds = pd["drawSettings"];
+		auto pdColors = pdds["colors"];
+		std::vector<Color> colors;
+		for (const auto& entry : pdColors) {
+			std::string hexString = entry.get<std::string>();
+			colors.emplace_back(std::strtol(hexString.c_str(), nullptr, 0));
+		}
+		ds.colors = colors;
+		ds.whiten = pdds["whiten"];
+
+		simplesets::PartitionSettings ps;
+		const auto& pdps = pd["partitionSettings"];
+		ps.banks = pdps["banks"];
+		ps.islands = pdps["islands"];
+		ps.regularityDelay = pdps["regularityDelay"];
+		ps.intersectionDelay = pdps["intersectionDelay"];
+		ps.admissibleRadiusFactor = pdps["admissibleRadiusFactor"];
+
+		simplesets::ComputeDrawingSettings cds;
+		const auto& pdcds = pd["computeDrawingSettings"];
+		cds.smooth = pdcds["smooth"];
+		cds.cutoutRadiusFactor = pdcds["cutoutRadiusFactor"];
+		cds.smoothingRadiusFactor = pdcds["smoothingRadiusFactor"];
+
+		double cover = pd["cover"];
+
+		// Partition
+		auto partitions = simplesets::partition(points, gs, ps, 8 * CGAL::to_double(gs.dilationRadius()));
+
+		// Draw
+		simplesets::Partition* thePartition;
+		bool found = false;
+		for (auto& [time, partition] : partitions) {
+			if (time < cover * gs.dilationRadius()) {
+				thePartition = &partition;
+				found = true;
+			}
+		}
+		simplesets::Partition& partition = found ? (*thePartition) : partitions.front().second;
+
+		bool wellSeparated = true;
+		for (const auto& p : points) {
+			for (const auto& q : points) {
+				if (p.category == q.category) continue;
+				if (CGAL::squared_distance(p.point, q.point) < 4 * gs.pointSize * gs.pointSize) {
+					wellSeparated = false;
+				}
+			}
+		}
+		if (wellSeparated) {
+			auto dpd = simplesets::DilatedPatternDrawing(partition, gs, cds);
+			auto ssPainting = simplesets::SimpleSetsPainting(dpd, ds);
+			auto pr = std::make_shared<renderer::PaintingRenderer>();
+			ssPainting.paint(*pr);
+			painting = pr;
+		} else {
+			std::cerr << "Points of different category are too close together; not computing a drawing." << std::endl;
+		}
+	} else if (projectData["type"] == "chorematic_map") {
+
 	} else {
 		std::cerr << "Unknown type \"" << projectData["type"] << "\" specified\n";
 	}
